e5b6dda1f3e969ede5e93818e92def12defd81e3
Spypaste <spypaste@outlook.jp>
2017-03-29 22:33:45 +0900

    Update Main class

--- src/main/java/net/xyz/spypaste/easylock/Main.java
+++ src/main/java/net/xyz/spypaste/easylock/Main.java
@@ -6,13 +6,18 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Created by Spypaste on 2017/03/29.
  */
 public class Main extends JavaPlugin{
+    private static Main instance;
     @Override
     public void onEnable() {
-
+        instance = this;
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static Main getInstance() {
+        return instance;
     }
 }
