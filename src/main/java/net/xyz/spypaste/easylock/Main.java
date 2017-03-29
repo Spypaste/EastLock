5f220eaedca7244035bc6df18675034a7e73e637
Spypaste <spypaste@outlook.jp>
2017-03-29 22:30:24 +0900

    FirstCommit(メインクラス、plugin.ymlの作成)

---
+++ src/main/java/net/xyz/spypaste/easylock/Main.java
@@ -0,0 +1,18 @@
+package net.xyz.spypaste.easylock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Spypaste on 2017/03/29.
+ */
+public class Main extends JavaPlugin{
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
