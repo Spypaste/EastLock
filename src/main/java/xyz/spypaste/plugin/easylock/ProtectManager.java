95ff3285e32ed99a4d79de206578a5c9e9921144
Spypaste <spypaste@outlook.jp>
2017-03-30 16:41:05 +0900

    不要なファイルまでコミットしていたので削除

--- src/main/java/xyz/spypaste/plugin/easylock/ProtectManager.java
+++
@@ -1,8 +0,0 @@
-package xyz.spypaste.plugin.easylock;
-
-/**
- * Created by Spypaste on 2017/03/29.
- */
-public class ProtectManager {
-
-}
--- src/main/java/xyz/spypaste/plugin/easylock/database/Mysql.java
+++
@@ -1,7 +0,0 @@
-package xyz.spypaste.plugin.easylock.database;
-
-/**
- * Created by Spypaste on 2017/03/29.
- */
-public class Mysql {
-}
